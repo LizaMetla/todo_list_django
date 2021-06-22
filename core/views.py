3dee4402289b4c86bd3adda1c0e90fc71a7e613b
Liza <liza.metla@gmail.com>
2021-06-22 11:41:41 +0300

    initial commit

---
+++ core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView
+from django.views.generic.list import ListView
+from .models import Task
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'core/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
